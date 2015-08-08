257bbdf290e922870e6fd553c139ad8e8abce6e5
Marc Egea i Sala <marc.egea.sala@gmail.com>
2015-08-09 00:04:14 +0200

    Finish game when there's no plays remaining

--- src/stores/GameStore.js
+++ src/stores/GameStore.js
@@ -1,14 +1,24 @@
-import Alt from '../Alt';
-import shuffle from 'shuffle-array';
+import Alt         from '../Alt';
+import GameActions from '../actions/GameActions';
+import shuffle     from 'shuffle-array';
 
 class GameStore {
   constructor() {
+    this.bindActions(GameActions);
+
     this._values = shuffle(Array.apply(0, Array(100)).map(function(x, y) { return y + 1 }));
 
     this.state = {
-      remainingPlays: 100,
-      nextPlay      : this._values.pop()
+      remaining: 100,
+      nextPlay : this._values.pop(),
+      finished : false
     };
+  }
+
+  onPlay() {
+    this.state.remaining--;
+
+    this.state.finished = this.state.remaining === 0;
   }
 }
 
--- test/stores/GameStore.js
+++ test/stores/GameStore.js
@@ -1,6 +1,7 @@
-import Alt       from 'src/Alt';
-import GameStore from 'src/stores/GameStore';
-import {assert}  from 'chai';
+import Alt         from 'src/Alt';
+import GameStore   from 'src/stores/GameStore';
+import GameActions from 'src/actions/GameActions';
+import {assert}    from 'chai';
 
 describe('GameStore', () => {
   // Restore all stores to original state before each test.
@@ -9,9 +10,26 @@ describe('GameStore', () => {
   });
 
   it('sets up instance properties correctly', () => {
-    assert.equal(GameStore.getState().remainingPlays, 100);
+    assert.equal(GameStore.getState().remaining, 100);
+    assert.equal(GameStore.getState().finished, false);
     assert.isNumber(GameStore.getState().nextPlay);
     assert.isAbove(GameStore.getState().nextPlay, 0);
     assert.isBelow(GameStore.getState().nextPlay, 101);
+  });
+
+  it('decreases the number of plays as game goes along', () => {
+    assert.equal(GameStore.getState().remaining, 100);
+    Alt.dispatcher.dispatch({ action: GameActions.PLAY, data: 0 });
+    assert.equal(GameStore.getState().remaining, 99);
+    Alt.dispatcher.dispatch({ action: GameActions.PLAY, data: 0 });
+    assert.equal(GameStore.getState().remaining, 98);
+    assert.equal(GameStore.getState().finished, false);
+  });
+
+  it('finish game after 100 plays', () => {
+    for(let i = 0; i < 100; i++) {
+      Alt.dispatcher.dispatch({ action: GameActions.PLAY, data: 0 });
+    }
+    assert.equal(GameStore.getState().finished, true);
   });
 });
