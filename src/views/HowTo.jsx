b9db43956b3a9622cc29bff608d930d17627cf39
Marc Egea i Sala <marc.egea.sala@gmail.com>
2015-08-10 07:56:09 +0200

    Improve description

--- src/views/HowTo.jsx
+++ src/views/HowTo.jsx
@@ -5,8 +5,9 @@ export default React.createClass({
     return(
       <div id="howto">
         <p>
-          <b>HOW TO PLAY</b>: You have 100 numbers. Put each number in a square to add its value to cell value.
-          Try to add multiples of 3,5 or 15 to score points!
+          <b>HOW TO PLAY</b>: 
+          You have 100 numbers. Put each number in a square to add there its value.
+          Try to achieve multiples of 3, 5 or 15 to score points!
         </p>
       </div>
     );
